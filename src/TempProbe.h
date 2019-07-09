d4632d9cb70eabbd526db7c3db9f94710e25409a
Brett L Kleinschmidt <blk@blk.me>
2019-07-08 21:24:53 -0700

    Initial commit

---
+++ src/TempProbe.h
@@ -0,0 +1,103 @@
+/*
+ * TempProbe.h
+ */
+
+ #define ADC_BIT_DEPTH 10
+ #define ADC_OHM_VALUE 2.2e4
+
+ class TempProbe {
+ private:
+   struct SteinhartCoefficients {
+     float A, B, C;
+   };
+
+   typedef enum {
+     STATUS_NONE,
+     STATUS_OK,
+   } temp_status_t;
+
+   int _pin;
+   float _temp;
+   float _tempAvg;
+   bool _hasAvg;
+   float _offset;
+   temp_status_t _status;
+   SteinhartCoefficients _s = {7.3431401e-4, 2.1574370e-4, 9.5156860e-8};
+
+ public:
+   TempProbe(const unsigned char pin);
+
+   void Process();
+   float getTemp();
+   float getTempAvg();
+   temp_status_t getStatus();
+
+ private:
+   void calcTemp();
+   void calcTempAvg();
+   void setTemp(const float temp);
+   void setStatus(const temp_status_t status);
+ };
+
+ TempProbe::TempProbe(const unsigned char pin) :
+  _pin(pin),
+  _temp(0.0f),
+  _tempAvg(0),
+  _hasAvg(false),
+  _offset(0.0f),
+  _status(STATUS_NONE) {
+ }
+
+ void TempProbe::Process() {
+   calcTemp();
+   calcTempAvg();
+ }
+
+ constexpr float celsiusToFahrenheit(const float tempC) {
+   return 32.0f + ((9.0f / 5.0f) * tempC);
+ }
+
+ void TempProbe::setTemp(const float temp) {
+   _temp = celsiusToFahrenheit(temp);
+   _temp += _offset;
+   setStatus(STATUS_OK);
+ }
+
+ void TempProbe::setStatus(const temp_status_t status){
+   _status = status;
+ }
+
+ float TempProbe::getTemp() {
+   return _temp;
+ }
+
+ float TempProbe::getTempAvg() {
+   return _tempAvg;
+ }
+
+ void TempProbe::calcTemp() {
+   static_assert(ADC_BIT_DEPTH <= (8 * sizeof(int)), "ADC bit depth too large");
+   static constexpr int amax = (1 << ADC_BIT_DEPTH) - 1;
+
+   float aval = analogRead(_pin);
+   if (aval == 0 || aval == amax) {
+     setStatus(STATUS_NONE);
+     return;
+   }
+
+   const float lnR = log(ADC_OHM_VALUE / (((float) amax / aval) - 1.0f));
+   const float tempK = 1.0f / ((_s.A) + (_s.B * lnR) + (_s.C * lnR * lnR * lnR));
+
+   setTemp(tempK - 273.15f);
+ }
+
+ void TempProbe::calcTempAvg() {
+   if (_status == STATUS_OK) {
+     if (!_hasAvg) {
+       _tempAvg = _temp;
+       _hasAvg = true;
+     } else {
+       _tempAvg += (2.0f / (1.0f + 60.0f)) * (_temp - _tempAvg);
+     }
+   }
+ }
---
+++ src/main.cpp
@@ -0,0 +1,111 @@
+#include <Arduino.h>
+#include <ELClient.h>
+#include <ELClientMqtt.h>
+#include <TempProbe.h>
+
+#define DEBUG 1
+#define READ_PERIOD 1000 //ms
+#define READS_PER_PUBLISH 5
+
+ELClient esp(&Serial, &Serial);
+ELClientMqtt mqtt(&esp);
+
+struct MetaProbe {
+  TempProbe sensor;
+  String topic;
+};
+
+const size_t probesLen = 4;
+MetaProbe probes[probesLen] = {
+  {TempProbe(A0), "smoker/probe/1/temp"},
+  {TempProbe(A1), "smoker/probe/2/temp"},
+  {TempProbe(A2), "smoker/probe/3/temp"},
+  {TempProbe(A3), "smoker/probe/4/temp"},
+};
+
+void publishProbe(MetaProbe *probe) {
+  const String value = String(probe->sensor.getTempAvg());
+  const String topic = probe->topic;
+  if (DEBUG) {
+    Serial.print(topic);
+    Serial.print(" -> ");
+    Serial.print(value);
+    Serial.println();
+  }
+  mqtt.publish(topic.c_str(), value.c_str());
+}
+
+void wifiCb(void* response) {
+  ELClientResponse *res = (ELClientResponse*)response;
+  if (res->argc() == 1) {
+    uint8_t status;
+    res->popArg(&status, 1);
+
+    if(status == STATION_GOT_IP) {
+      Serial.println("WiFi connected");
+    } else {
+      Serial.print("WiFi not ready: ");
+      Serial.println(status);
+    }
+  }
+}
+
+bool connected;
+
+// Callback when MQTT is connected
+void mqttConnected(void* response) {
+  Serial.println("MQTT connected!");
+  connected = true;
+}
+
+// Callback when MQTT is disconnected
+void mqttDisconnected(void* response) {
+  Serial.println("MQTT disconnected");
+  connected = false;
+}
+
+void mqttPublished(void* response) {
+  Serial.println("MQTT published");
+}
+
+void setup() {
+  Serial.begin(38400);
+  Serial.println("EL-Client starting!");
+
+  // Sync-up with esp-link, this is required at the start of any sketch and initializes the
+  // callbacks to the wifi status change callback. The callback gets called with the initial
+  // status right after Sync() below completes.
+  esp.wifiCb.attach(wifiCb); // wifi status change callback, optional (delete if not desired)
+  bool ok;
+  do {
+    ok = esp.Sync();      // sync up with esp-link, blocks for up to 2 seconds
+    if (!ok) Serial.println("EL-Client sync failed!");
+  } while(!ok);
+  Serial.println("EL-Client synced!");
+
+  // Set-up callbacks for events and initialize with es-link.
+  mqtt.connectedCb.attach(mqttConnected);
+  mqtt.disconnectedCb.attach(mqttDisconnected);
+  mqtt.publishedCb.attach(mqttPublished);
+  mqtt.setup();
+
+  Serial.println("EL-MQTT ready");
+}
+
+static int count = 0;
+static uint32_t last;
+
+void loop() {
+  esp.Process();
+  if (connected && (millis() - last) > READ_PERIOD) {
+    count = (count + 1) % READS_PER_PUBLISH;
+    for (size_t i=0; i<probesLen; ++i) {
+      probes[i].sensor.Process();
+      if (count == 0) {
+        publishProbe(&probes[i]);
+      }
+      delay(50);
+    }
+    last = millis();
+  }
+}
